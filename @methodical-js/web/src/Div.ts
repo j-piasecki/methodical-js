8a1c6a154819fc56b085fc57a344e969059bfcee
Kuba <jakubpiasecki67@gmail.com>
2024-01-06 15:53:40 +0100

    Add web hook and simple `Div` implementation

---
+++ @methodical-js/web/src/Div.ts
@@ -0,0 +1,74 @@
+import { WorkingTree, ViewNode, ViewNodeManager } from '@methodical-js/core'
+import { ViewConfig } from './ViewConfig.js'
+
+interface DivConfig extends ViewConfig {
+  style?: Partial<CSSStyleDeclaration>
+  onClick?: (event: MouseEvent) => void
+}
+
+const viewManager: ViewNodeManager = {
+  createView(node: ViewNode) {
+    console.log('create', node.id)
+
+    const view = document.createElement('div')
+    view.id = node.id as string
+    
+    const config = node.config as DivConfig
+
+    if (config.style !== undefined) {
+      Object.assign(view.style, config.style)
+    }
+
+    if (config.onClick !== undefined) {
+      view.addEventListener('click', config.onClick)
+    }
+
+    if (node.parent !== undefined) {
+      const parentView = (node.parent as ViewNode).viewReference as HTMLElement | undefined
+      parentView?.appendChild(view)
+    }
+
+    node.viewReference = view
+  },
+
+  dropView(node: ViewNode) {
+    console.log('drop', node.id)
+
+    const view = node.viewReference as HTMLElement | undefined
+    view?.remove()
+  },
+
+  updateView(oldNode: ViewNode, newNode: ViewNode) {
+    const view = oldNode.viewReference as HTMLDivElement | undefined
+    if (view === undefined) {
+      return
+    }
+
+    newNode.viewReference = view
+    console.log('update', oldNode.id, newNode.id)
+
+    const oldConfig = oldNode.config as DivConfig
+    const newConfig = newNode.config as DivConfig
+
+    if (oldConfig.onClick !== newConfig.onClick) {
+      if (oldConfig.onClick !== undefined) {
+        view.removeEventListener('click', oldConfig.onClick)
+      }
+
+      if (newConfig.onClick !== undefined) {
+        view.addEventListener('click', newConfig.onClick)
+      }
+    }
+
+    if (newConfig.style !== undefined) {
+      Object.assign(view.style, newConfig.style)
+    }
+  },
+}
+
+export const Div = (config: DivConfig, body?: () => void) => {
+  config.__viewType = 'div'
+
+  const view = WorkingTree.createViewNode(config, viewManager, body)
+  return view
+}
---
+++ @methodical-js/web/src/index.ts
@@ -0,0 +1,28 @@
+import { WorkingTree } from '@methodical-js/core'
+
+export const remember = <T>(value: T) => {
+  const rememberedNode = WorkingTree.createRememberNode(value)
+  return rememberedNode.value
+}
+
+export const sideEffect = (effect: () => void, ...dependencies: unknown[]) => {
+  return WorkingTree.createEffectNode(effect, dependencies)
+}
+
+const Methodical = {
+  init: (rootView: HTMLElement) => {
+    WorkingTree.setRootViewReference(rootView);
+    WorkingTree.performInitialRender();
+
+    function render() {
+      WorkingTree.performUpdate();
+      requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+  }
+};
+
+export default Methodical;
+
+export { Div } from './Div.js'
