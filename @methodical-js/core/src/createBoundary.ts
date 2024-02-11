b6a98f25eb2282a98b6da8d8c941af3f37debc30
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-02-11 10:18:53 +0100

    Add `createBoundary`

---
+++ @methodical-js/core/src/createBoundary.ts
@@ -0,0 +1,29 @@
+import { BaseConfig } from './BaseConfig.js'
+import { WorkingTree } from './WorkingTree.js'
+import { ViewNodeManager } from './ViewNodeManager.js'
+
+type BoundaryFunction = <C extends BaseConfig>(config: C, ...args: unknown[]) => void
+
+const viewManager: ViewNodeManager = {
+  createView(node) {
+    node.viewReference = node.id
+  },
+
+  dropView(_node) {
+    // noop
+  },
+
+  updateView(_oldNode, _newNode) {
+    // noop
+  },
+}
+
+export function createBoundary<C extends BaseConfig>(target: BoundaryFunction) {
+  return function (config: C, ...args: unknown[]) {
+    const body = () => {
+      target(config, ...args)
+    }
+
+    return WorkingTree.createViewNode(config, viewManager, body)
+  }
+}
