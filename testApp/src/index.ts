9b7205dd61d19de7764b26815e6218e4e2e92145
Kuba <jakubpiasecki67@gmail.com>
2024-02-17 22:35:45 +0100

    Update app

--- testApp/src/index.ts
+++ testApp/src/index.ts
@@ -1,5 +1,5 @@
-import { BaseConfig, createBoundary } from '@methodical-js/core'
-import Methodical, { remember, Div, on } from '@methodical-js/web'
+import { BaseConfig, SuspenseBoundary, createBoundary, defer, suspend } from '@methodical-js/core'
+import Methodical, { remember, Div, on, Text } from '@methodical-js/web'
 import { Tracing } from '@methodical-js/core'
 
 const saveTemplateAsFile = (filename: string, dataObjToWrite: any) => {
@@ -24,89 +24,234 @@ const Test = createBoundary((config: BaseConfig, test: string) => {
   console.log('Test', test)
 })
 
-Div(
-  {
-    id: 'test',
-    style: { width: '500px', height: '500px', backgroundColor: 'red' },
-  },
-  () => {
-    const backgroundColor = remember('blue')
-    const innerBackgroundColor = remember('red')
+function Squares() {
+  Div(
+    {
+      id: 'test',
+      style: { width: '500px', height: '500px', backgroundColor: 'red' },
+    },
+    () => {
+      const backgroundColor = remember('blue')
+      const innerBackgroundColor = remember('red')
 
-    Div(
-      {
-        id: 'test2',
-        style: { width: '300px', height: '300px', backgroundColor: backgroundColor.value },
-      },
-      () => {
-        const bg = backgroundColor.value
-        on(
-          'click',
-          () => {
-            if (bg === 'blue') {
-              backgroundColor.value = 'green'
-            } else {
-              backgroundColor.value = 'blue'
-            }
-          },
-          bg
-        )
+      Text({ id: 'text', value: backgroundColor.value })
+      Div(
+        {
+          id: 'test2',
+          style: { width: '300px', height: '300px', backgroundColor: backgroundColor.value },
+        },
+        () => {
+          const bg = backgroundColor.value
+          on(
+            'click',
+            () => {
+              if (bg === 'blue') {
+                backgroundColor.value = 'green'
+              } else {
+                backgroundColor.value = 'blue'
+              }
+            },
+            bg
+          )
 
-        if (backgroundColor.value === 'blue') {
+          if (backgroundColor.value === 'blue') {
+            Div(
+              {
+                id: 'test5',
+                style: {
+                  width: '100px',
+                  height: '100px',
+                  backgroundColor: innerBackgroundColor.value,
+                },
+              },
+              () => {
+                on('click', (e: any) => {
+                  e.stopPropagation()
+                  // if (innerBackgroundColor.value === 'red') {
+                  //   innerBackgroundColor.value = 'magenta'
+                  // } else {
+                  //   innerBackgroundColor.value = 'red'
+                  // }
+
+                  if (Tracing.enabled) {
+                    const trace = Tracing.stop()
+
+                    saveTemplateAsFile('trace.json', trace)
+                  } else {
+                    Tracing.start()
+                  }
+                })
+
+                Test({ id: 'test6' }, 'napis')
+              }
+            )
+          }
+        }
+      )
+
+      Div(
+        {
+          id: 'test3',
+          style: { width: '200px', height: '200px', backgroundColor: 'magenta' },
+          pure: true,
+        },
+        () => {
+          console.log('test3 execute')
           Div(
             {
-              id: 'test5',
-              style: {
-                width: '100px',
-                height: '100px',
-                backgroundColor: innerBackgroundColor.value,
-              },
+              id: 'test4',
+              style: { width: '100px', height: '100px', backgroundColor: 'yellow' },
             },
             () => {
-              on('click', (e: any) => {
-                e.stopPropagation()
-                // if (innerBackgroundColor.value === 'red') {
-                //   innerBackgroundColor.value = 'magenta'
-                // } else {
-                //   innerBackgroundColor.value = 'red'
-                // }
-
-                if (Tracing.enabled) {
-                  const trace = Tracing.stop()
-
-                  saveTemplateAsFile('trace.json', trace)
-                } else {
-                  Tracing.start()
-                }
-              })
-
-              Test({ id: 'test6' }, 'napis')
+              console.log('test4 execute')
             }
           )
         }
-      }
-    )
+      )
+    }
+  )
+}
 
-    Div(
-      {
-        id: 'test3',
-        style: { width: '200px', height: '200px', backgroundColor: 'magenta' },
-        pure: true,
+let suspenseCounter = 0
+let deferCounter = 0
+
+function fetchSuspend() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(suspenseCounter++)
+    }, 2000)
+  })
+}
+
+function fetchDefer() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(deferCounter++)
+    }, 2000)
+  })
+}
+
+function SuspendTest() {
+  Div(
+    {
+      id: 'root',
+      style: {
+        width: '1200px',
+        height: '500px',
       },
-      () => {
-        console.log('test3 execute')
-        Div(
-          {
-            id: 'test4',
-            style: { width: '100px', height: '100px', backgroundColor: 'yellow' },
+    },
+    () => {
+      Div(
+        {
+          id: 'suspendcontainer',
+          style: {
+            width: '1200px',
+            height: '250px',
+            backgroundColor: 'yellow',
           },
-          () => {
-            console.log('test4 execute')
-          }
-        )
-      }
-    )
+        },
+        () => {
+          const dataId = remember(0)
+          SuspenseBoundary(
+            { id: 'suspense' },
+            () => {
+              const value = suspend(fetchSuspend, dataId.value)
+              Text({ id: 'value', value: `currentValue: ${value}` })
+
+              Div(
+                {
+                  id: 'button',
+                  style: {
+                    width: '100px',
+                    height: '50px',
+                    backgroundColor: 'red',
+                  },
+                },
+                () => {
+                  on('click', () => {
+                    dataId.value++
+                  })
+
+                  Text({ id: 'text', value: 'fetch next' })
+                }
+              )
+            },
+            () => {
+              Text({ id: 'fallback', value: 'Suspended' })
+            }
+          )
+        }
+      )
+
+      Div(
+        {
+          id: 'defercontainer',
+          style: {
+            width: '1200px',
+            height: '250px',
+            backgroundColor: 'yellow',
+          },
+        },
+        () => {
+          SuspenseBoundary(
+            { id: 'suspense' },
+            () => {
+              const dataId = remember(0)
+              const value = defer(fetchDefer, dataId.value)
+              Text({ id: 'value', value: `currentValue: ${value}` })
+              Div(
+                {
+                  id: 'button',
+                  style: {
+                    width: '100px',
+                    height: '50px',
+                    backgroundColor: 'red',
+                  },
+                },
+                () => {
+                  on('click', () => {
+                    dataId.value++
+                  })
+                  Text({ id: 'text', value: 'fetch next' })
+                }
+              )
+            },
+            () => {
+              Text({ id: 'fallback', value: 'Suspended' })
+            }
+          )
+        }
+      )
+    }
+  )
+}
+
+Div({ id: 'app' }, () => {
+  const showSquares = remember(true)
+
+  Div(
+    {
+      id: 'switcher',
+      style: {
+        width: '100px',
+        height: '50px',
+        backgroundColor: 'blue',
+      },
+    },
+    () => {
+      on('click', () => {
+        showSquares.value = !showSquares.value
+      })
+
+      Text({ id: 'text', value: 'Show the other thing' })
+    }
+  )
+
+  if (showSquares.value) {
+    Squares()
+  } else {
+    SuspendTest()
   }
-)
+})
 
 Methodical.init(document.getElementById('app')!)
