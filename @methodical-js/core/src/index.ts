1147df67fe0a89a0ec601db2ab69968d7a8b19a1
Jakub Piasecki <jakubpiasecki67@gmail.com>
2024-02-24 13:47:39 +0100

    Add ambient

--- @methodical-js/core/src/index.ts
+++ @methodical-js/core/src/index.ts
@@ -1,7 +1,3 @@
-import { BaseConfig } from './BaseConfig.js'
-import { EventNodeManager } from './EventNodeManager.js'
-import { WorkingTree } from './WorkingTree.js'
-
 export { NodeType } from './NodeType.js'
 export { WorkingTree } from './WorkingTree.js'
 export { BaseConfig } from './BaseConfig.js'
@@ -16,32 +12,5 @@ export { Tracing } from './Tracing.js'
 
 export { createBoundary } from './createBoundary.js'
 
-export const remember = <T>(value: T) => {
-  const rememberedNode = WorkingTree.createRememberNode(value)
-  return rememberedNode.value
-}
-
-export const sideEffect = (effect: () => void, ...dependencies: unknown[]) => {
-  return WorkingTree.createEffectNode(effect, dependencies)
-}
-
-export const eventHandler = <T, U>(
-  name: string,
-  handler: (event: T) => void,
-  eventManager: EventNodeManager<U>,
-  ...dependencies: unknown[]
-) => {
-  return WorkingTree.createEventNode(name, handler, eventManager, dependencies)
-}
-
-export const suspend = <T>(fun: () => Promise<T>, ...dependencies: unknown[]) => {
-  return WorkingTree.createSuspedNode(fun, dependencies).value
-}
-
-export const defer = <T>(fun: () => Promise<T>, ...dependencies: unknown[]) => {
-  return WorkingTree.createDeferNode(fun, dependencies).value
-}
-
-export const SuspenseBoundary = (config: BaseConfig, body?: () => void, fallback?: () => void) => {
-  return WorkingTree.createSuspenseBoundaryNode(config, body, fallback)
-}
+export { remember, sideEffect, eventHandler, suspend, defer, SuspenseBoundary } from './effects.js'
+export { createAmbient, readAmbient } from './Ambient.js'
