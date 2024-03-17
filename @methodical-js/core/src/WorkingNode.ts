bcfbf01b67ee9c37e27d699894bc2455a1c455ef
Kuba <jakubpiasecki67@gmail.com>
2024-03-17 15:37:11 +0100

    Simplify finding predecessor

--- @methodical-js/core/src/WorkingNode.ts
+++ @methodical-js/core/src/WorkingNode.ts
@@ -29,23 +29,7 @@ export class WorkingNode {
     const currentView = this.parent as ViewNode
 
     if (currentView.predecessorNode !== undefined) {
-      // try finding the path up to the previous context
-      let predecessor: WorkingNode | undefined = currentView
-      const path: (string | number)[] = [this.id]
-
-      while (predecessor !== undefined && predecessor.id !== currentView.predecessorNode!.id) {
-        path.unshift(predecessor.id)
-        predecessor = predecessor.parent
-      }
-
-      // if predecessor is undefined, it means that the previous context is not a parent of the current view
-      // otherwise, assuming the node at the found path existed, we have found the corresponding node in the previous context
-      const previousNode =
-        predecessor === undefined
-          ? undefined
-          : (currentView.predecessorNode!.getNodeFromPath(path) as typeof this | undefined)
-
-      return previousNode
+      return currentView.predecessorNode.getNodeFromPath([this.id]) as typeof this | undefined
     }
 
     return undefined
